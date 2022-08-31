fa9191a750d8a8e37fc71f81a56328f1fde4b24f
HyoinJeong <hiamy0107@naver.com>
2022-08-31 16:41:12 +0900

    내가 만든 예외 filter(AccessDeniedException)를 ExceptionTranslationFilter를 사용하여 처리

--- ssmc/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,16 +1,30 @@
 package com.prgrms.devcourse.configures;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -30,6 +44,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     http
       .authorizeRequests()
         .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+        .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
         .anyRequest().permitAll()
         .and()
       .formLogin()
@@ -49,10 +64,26 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
       .requiresChannel()
             .anyRequest().requiresSecure()
             .and()
-      . anonymous()
-            .principal("thisIsAnonymousUser")
-            .authorities("ROLE_ANONYMOUS","ROLE_UNKNOWN")
+      /**
+      * 예외처리 핸들러
+      */
+      .exceptionHandling()
+      .accessDeniedHandler(accessDeniedHandler())
     ;
+  }
+
+  @Bean
+  public AccessDeniedHandler accessDeniedHandler() {
+    return (request, response, e) -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      Object principal = authentication != null ? authentication.getPrincipal() : null;
+      log.warn("{} is denied", principal, e);
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      response.setContentType("text/plain;charset=UTF-8");
+      response.getWriter().write("ACCESS DENIED");
+      response.getWriter().flush();
+      response.getWriter().close();
+    };
   }
 
 }
